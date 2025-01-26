1d32fcdfcbefa35d23bbf7c56d5a5b22d292b7cb
Harsh Kumar <harsh.kumar@intimetec.com>
2025-01-26 19:36:08 +0530

    replaced firestore backend url from environment file

--- src/environment.ts
+++ src/environment.ts
@@ -1,26 +1,28 @@
-export const environment = {
-    production: false,
-    firebase: {
-        apiKey: 'AIzaSyApnyuVkb2Rai916adVCX_1VhOTuAn79y4',
-        authDomain: 'doublehash-e9828.firebaseapp.com',
-        databaseURL: 'https://doublehash-e9828-default-rtdb.firebaseio.com',
-        projectId: 'doublehash-e9828',
-        storageBucket: 'doublehash-e9828.appspot.com',
-        messagingSenderId: '910190376570',
-    },
-};
+//for doublehash test database
+// export const environment = {
+// production: false,
+// firebase: {
+//     apiKey: 'AIzaSyApnyuVkb2Rai916adVCX_1VhOTuAn79y4',
+//     authDomain: 'doublehash-e9828.firebaseapp.com',
+//     databaseURL: 'https://doublehash-e9828-default-rtdb.firebaseio.com',
+//     projectId: 'doublehash-e9828',
+//     storageBucket: 'doublehash-e9828.appspot.com',
+//     messagingSenderId: '910190376570',
+// },
+// };
 
 // appId: '1:910190376570:web:a6c44a2d25a5902344edc3',
 // measurementId: 'G-6V687E2V7P',
 
-// export const environment = {
-//     production: false,
-//     firebase: {
-//         apiKey: 'AIzaSyD1X6A4zLP106PwjBlicRzVpgsvVVhO1OM',
-//         authDomain: 'doublehash-e86c1.firebaseapp.com',
-//         databaseURL: 'https://doublehash-e86c1-default-rtdb.firebaseio.com',
-//         projectId: 'doublehash-e86c1',
-//         storageBucket: 'doublehash-e86c1.appspot.com',
-//         messagingSenderId: '736519290690',
-//     },
-// };
+// for doublehast database
+export const environment = {
+    production: false,
+    firebase: {
+        apiKey: 'AIzaSyD1X6A4zLP106PwjBlicRzVpgsvVVhO1OM',
+        authDomain: 'doublehash-e86c1.firebaseapp.com',
+        databaseURL: 'https://doublehash-e86c1-default-rtdb.firebaseio.com',
+        projectId: 'doublehash-e86c1',
+        storageBucket: 'doublehash-e86c1.appspot.com',
+        messagingSenderId: '736519290690',
+    },
+};
